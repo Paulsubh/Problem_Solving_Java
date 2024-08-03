584797113214525c15f9c6d1419b68e524abada9
Paulsubh <subhajitp4321@gmail.com>
2024-08-03 19:40:59 +0530

    2nd commit-added new file

---
+++ Strings/AnagramString.java
@@ -0,0 +1,35 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class AnagramString {
+    public static String SortString(String s){
+        char ch[]=s.toCharArray();
+        Arrays.sort(ch);
+        return new String(ch);
+    }
+
+public static boolean checkAnagramString(String str1, String str2 ){
+    if(str1.length()!=str2.length()){
+        return false;
+    }
+    str1=SortString(str1);
+    str2=SortString(str2);
+    for(int i=0;i<str1.length();i++){
+        if(str1.charAt(i)!=str2.charAt(i)){
+            return false;
+        }
+    }
+        return true;
+}
+public static void main(String[] args) {
+    String str1="CAT";
+    String str2="ACT";
+    boolean isAnagram=checkAnagramString(str1, str2);
+    if(isAnagram){
+        System.out.println("The strings are in anagram.");
+    }else{
+        System.out.println("The strings  are not  in anagram.");
+    }
+  }
+}
