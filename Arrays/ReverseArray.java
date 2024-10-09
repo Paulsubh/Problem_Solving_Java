eec7f721c2226db0261ac0da5440b5d1c571d16e
Paulsubh <subhajitp4321@gmail.com>
2024-10-09 12:23:50 +0530

    9-10-24

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,24 +1,27 @@
 package Arrays;
 
 public class ReverseArray {
-    public static void printArray(int arr[],int n){
-        System.out.println("The reversed array is: ");
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+ " ");
+    public static int[] reverseArray(int arr[]){
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while (start < end) {
+            // Swap the elements at start and end positions
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-    }
-    public static void reverseArray(int ans[],int n){
-        int rev[] =new int[n];
-        for(int i=n-1;i>=0;i--){
-            rev[n-i-1]=ans[i];
-        }
-        printArray(rev,n);
+     return arr;
     }
 
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
-        int n=5;
-        reverseArray(arr, n);
+        reverseArray(arr);
+        for(int e:arr){
+            System.out.print(e+" ");
+        }
     }
     
 }
